8f1e4ed56d53d8b69b83834229ecf9ec62335e04
duke.lu <duke.lu@ideamake.cn>
2020-05-29 15:51:47 +0800

    feat: add test module

---
+++ instrumentality-spring-boot-test/src/main/java/cn/ibaleine/boot/instrumentalityspringboottest/InstrumentalitySpringBootTestApplication.java
@@ -0,0 +1,13 @@
+package cn.ibaleine.boot.instrumentalityspringboottest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstrumentalitySpringBootTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstrumentalitySpringBootTestApplication.class, args);
+    }
+
+}
