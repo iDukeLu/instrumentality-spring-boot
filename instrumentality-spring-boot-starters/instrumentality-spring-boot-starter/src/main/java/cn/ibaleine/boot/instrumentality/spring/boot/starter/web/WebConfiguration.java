bc5dd4ec26537eaf571ec5169686d01f6b368a14
duke.lu <duke.lu@ideamake.cn>
2020-06-06 23:03:27 +0800

    feat: add base stater module

---
+++ instrumentality-spring-boot-starters/instrumentality-spring-boot-starter/src/main/java/cn/ibaleine/boot/instrumentality/spring/boot/starter/web/WebConfiguration.java
@@ -0,0 +1,17 @@
+package cn.ibaleine.boot.instrumentality.spring.boot.starter.web;
+
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * TODO: 编辑说明信息
+ *
+ * @author luhongyu
+ * @date 2020.06.06
+ */
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+    }
+}
